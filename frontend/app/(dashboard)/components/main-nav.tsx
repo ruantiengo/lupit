f8c0e009f6bf73dd34f901bd662ca32dfb7839e3
Ruan Tiengo <ruan.rocha@contractor.rhimagnesita.com>
2024-02-17 13:10:54 -0300

    feat: add navbar title between routes bold

--- frontend/app/(dashboard)/components/main-nav.tsx
+++ frontend/app/(dashboard)/components/main-nav.tsx
@@ -1,34 +1,38 @@
 import Link from "next/link";
 
 import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 export function MainNav({
   className,
   ...props
 }: React.HTMLAttributes<HTMLElement>) {
+  const path = usePathname();
+  useEffect(() => {
+    console.log(path);
+  }, []);
+
+  const routes = [
+    { link: "/", title: "Dashboard" },
+    { link: "/teams", title: "Times" },
+    { link: "/players", title: "Jogadore" },
+  ];
   return (
     <nav
       className={cn("flex items-center space-x-4 lg:space-x-6", className)}
       {...props}
     >
-      <Link
-        href="/"
-        className="text-sm font-medium transition-colors hover:text-primary"
-      >
-        Dashboard
-      </Link>
-      <Link
-        href="/teams"
-        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-      >
-        Times
-      </Link>
-      <Link
-        href="/players"
-        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-      >
-        Jogadores
-      </Link>
+      {routes.map((r) => (
+        <Link
+          href={r.link}
+          className={`text-sm font-medium transition-colors hover:text-primary ${
+            path === r.link ? "" : "text-muted-foreground"
+          } `}
+        >
+          {r.title}
+        </Link>
+      ))}
     </nav>
   );
 }
