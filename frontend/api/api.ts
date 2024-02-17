dec232efb9100de457156322aefd77224fa3f3f9
Ruan Tiengo <ruan.rocha@contractor.rhimagnesita.com>
2024-02-17 18:08:42 -0300

    back api

--- frontend/api/api.ts
+++ frontend/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3003",
+  baseURL: "https://backend-production-4aac.up.railway.app/",
 });
 
 export default api;
