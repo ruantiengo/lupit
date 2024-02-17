c5d68da13334664521e6c7a352eaca92b4effa66
Ruan Tiengo <ruan.rocha@contractor.rhimagnesita.com>
2024-02-17 12:17:43 -0300

    feat: add position to column

--- frontend/app/players/components/columns.tsx
+++ frontend/app/players/components/columns.tsx
@@ -76,6 +76,22 @@ export const columns: ColumnDef<Player>[] = [
       );
     },
   },
+
+  {
+    accessorKey: "position",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Posição" />
+    ),
+    cell: ({ row }) => {
+      return (
+        <div className="flex space-x-2">
+          <span className="max-w-[500px] truncate font-medium ">
+            {row.getValue("position")}
+          </span>
+        </div>
+      );
+    },
+  },
   {
     accessorKey: "age",
     header: ({ column }) => (
